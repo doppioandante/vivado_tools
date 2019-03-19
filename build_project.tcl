bd0c1977ec59c275f2d8a18061a6c7e627cf205b
Enrico Lumetti <doppioandante@gmail.com>
2019-03-19 15:52:36 +0100

    Disable post-route synthesis

--- build_project.tcl
+++ build_project.tcl
@@ -79,6 +79,6 @@ wait_on_run synth_1
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 
-open_run impl_1
-write_vhdl -force post_impl/post_route.vhd
-write_sdf -force post_impl/post_route.sdf
+#open_run impl_1
+#write_vhdl -force post_impl/post_route.vhd
+#write_sdf -force post_impl/post_route.sdf
